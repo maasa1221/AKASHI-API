ab0952709d9cedb02a5a82ea78371e2a2e390ae9
maasa1221 <maasa1221@gmail.com>
2022-09-28 21:19:26 +0900

    変数修正

--- functions/akashi.js
+++ functions/akashi.js
@@ -1,14 +1,14 @@
 import fetch from "node-fetch";
 
-// const BASE_URL = process.env.BASE_URL;
-// const COMPANY_ID = process.env.COMPANY_ID;
-// const API_KEY = process.env.API_KEY;
-
-const BASE_URL = "https://atnd.ak4.jp/api/cooperation/" ;                                                    1260ms  水  9/28 21:11:57 2022
+const BASE_URL = "https://atnd.ak4.jp/api/cooperation/";
 const COMPANY_ID = "pkshatech";
 const API_KEY = "8cd303a8-272e-413d-8ce6-d7a9e83de2ec";
 
 export async function akashiClient(response, type) {
+  //   const BASE_URL = process.env.BASE_URL;
+  // const COMPANY_ID = process.env.COMPANY_ID;
+  // const API_KEY = process.env.API_KEY;
+
   const res = await fetch(
     `${BASE_URL}${COMPANY_ID}/stamps?token=${API_KEY}&type=${type}`,
     {
