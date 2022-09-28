167f581ee553d970b070606c1ee71d71007ccb83
maasa1221 <maasa1221@gmail.com>
2022-09-28 20:08:14 +0900

    環境変数化

---
+++ api/restFinish.js
@@ -0,0 +1,5 @@
+import { akashiClient } from "../functions/akashi.js";
+
+export default async function handler(_request, response) {
+  await akashiClient(32);
+}
---
+++ functions/akashi.js
@@ -0,0 +1,18 @@
+import fetch from "node-fetch";
+
+const BASE_URL = process.env.BASE_URL;
+const COMPANY_ID = process.env.COMPANY_ID;
+const API_KEY = process.env.API_KEY;
+
+export async function akashiClient(type) {
+  const res = await fetch(
+    `${BASE_URL}${COMPANY_ID}/stamps?token=${API_KEY}&type=${type}`,
+    {
+      method: "POST"
+    }
+  ).catch(e => {
+    console.error(e);
+  });
+  const data = await res.json();
+  return response.status(200).json({ data });
+}
